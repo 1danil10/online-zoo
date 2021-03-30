df85e5b8afeea04db230a8277fc82a08ef7b802a
1danil10 <danilmatvienko@gmail.com>
2021-03-30 19:31:16 +0300

    feat: add how it works and inspire blocks

--- gulpfile.js
+++ gulpfile.js
@@ -83,15 +83,17 @@ function watchFiles() {
 
 function handleHTML() {
   console.log(sourceFolder);
-  return src(path.src.html)
-    .pipe(
-      fileInclude({
-        basepath: "./" + sourceFolder + "/",
-      }),
-    )
-    .pipe(webpHTML())
-    .pipe(dest(path.build.html))
-    .pipe(browserSync.stream());
+  return (
+    src(path.src.html)
+      .pipe(
+        fileInclude({
+          basepath: "./" + sourceFolder + "/",
+        }),
+      )
+      //.pipe(webpHTML())
+      .pipe(dest(path.build.html))
+      .pipe(browserSync.stream())
+  );
 }
 
 function handleCSS() {
@@ -168,7 +170,7 @@ function handleFonts() {
   return src(path.src.fonts).pipe(ttf2woff2()).pipe(dest(path.build.fonts));
 }
 
-function fontsStyle(params) {
+async function fontsStyle(params) {
   let file_content = fs.readFileSync(sourceFolder + "/scss/base/fonts.scss");
   if (file_content == "") {
     fs.writeFile(sourceFolder + "/scss/base/fonts.scss", "", cb);
@@ -198,7 +200,7 @@ function fontsStyle(params) {
 
 function cb() {}
 
-gulp.task("svgSprite", function () {
+gulp.task("svgSprite", async function () {
   return src(path.src.icons)
     .pipe(
       svgSprite({
